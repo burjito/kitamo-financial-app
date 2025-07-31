81152a6e50947b9f4b168c1a3242237b4b862c25
Jedd Aguilar <jeddeishenaguilar@gmail.com>
2025-08-01 00:35:08 +0800

    feat: initial project setup with next.jsand tailwind css

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type {Metadata} from 'next';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster"
+
+export const metadata: Metadata = {
+  title: 'KitaMo | BPI Financial Planning',
+  description: 'Your personalized, AI-powered planning tool to explore life’s biggest financial “what-ifs” with clarity, confidence, and control.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased">
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
